03c3b4cc1c29fdfeb20bea7f48d8c7ca3171d7bd
CHARLES FERREIRA <charlesdccti@gmail.com>
2019-01-19 14:33:12 -0300

    Titulo em Barra de Navegação

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  lembretesApiUrl: 'https://deviup.com.br:3001/api'
+
 };
 
 /*
