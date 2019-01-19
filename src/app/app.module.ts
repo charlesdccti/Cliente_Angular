5f9627498168bcd29effdfcfd7671ae254b155ad
CHARLES FERREIRA <charlesdccti@gmail.com>
2019-01-19 17:09:41 -0300

    Criando a página de listagem de lembretes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorMsgComponent } from './compartilhado/error-msg/error-msg.component';
+import { ListaLembreteComponent } from './paginas/lista-lembrete/lista-lembrete.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ErrorMsgComponent
+    ErrorMsgComponent,
+    ListaLembreteComponent
   ],
   imports: [
     BrowserModule,
